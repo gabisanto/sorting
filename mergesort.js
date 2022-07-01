ad360366646aaa158d0adcc61f8e2d5782f07d07
gabisanto <gabrielasantoriello@gmail.com>
2022-07-01 15:56:25 -0300

    mergesort realizado

--- mergesort.js
+++ mergesort.js
@@ -6,10 +6,31 @@ function split(array) {
 }
 
 function mergeSort(array){
+    console.log('vuelta')
     if (array.length<2) return array
-    return split(array)
+    let [primera,segunda] = split(array)
+    console.log('primera: '+ primera)
+    console.log('segunda: '+ segunda)
+    let aux1 = mergeSort(primera)
+    console.log('mergeSort aux1 '+ aux1)
+    let aux2 = mergeSort(segunda)
+    console.log('mergeSort aux2 '+ aux2)
+    return merge(aux1,aux2)
+    //return split(array)
 
 }
 
+function merge(arr1,arr2){
+    let arrayOrdenado = []
+    while (arrayOrdenado.length < (arr1.length + arr2.length)) {
+        if(arr1[0] < arr2[0] || !arr2[0]){
+            arrayOrdenado.push(arr1.shift())
+        } else {
+            arrayOrdenado.push(arr2.shift())
+        }
+    }
+    return [...arrayOrdenado,...arr1,...arr2]
+}
 
-console.log(mergeSort([4,5,2,1,3,6,8,7]))
+//console.log(merge([5,6,7],[1,2,8]))
+console.log(mergeSort([6,5,4,3,2,1]))
